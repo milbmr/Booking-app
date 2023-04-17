76f9ca0c0a1dd228cdd3a08d13154c8582979393
miloud <miloamg@gmail.com>
2023-04-17 02:09:03 +0100

    responsive changes meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Rubik:wght@400;500;600;700&family=Marmelad&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="viewport"
+          content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi"
+        />
       </Head>
       <body>
         <Main />
