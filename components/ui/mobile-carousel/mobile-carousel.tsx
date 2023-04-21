f61efcd5b5d0d554f11ba16abc9aa5053dd17b21
miloud <miloamg@gmail.com>
2023-04-21 01:35:19 +0100

    responsive changes swiper 2

--- components/ui/mobile-carousel/mobile-carousel.tsx
+++ components/ui/mobile-carousel/mobile-carousel.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 import { ImageType } from "types";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,26 +10,32 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const MobileCarousel = ({ images }: { images: ImageType[] | undefined }) => {
+  /* const [prevEl, setPrevEl] = useState<HTMLElement | null>(null);
+  const [nextEl, setNextEl] = useState<HTMLElement | null>(null); */
   if (!images) return null;
   return (
-    <Swiper
-      modules={[Navigation, Pagination]}
-      navigation={true}
-      pagination={{ dynamicBullets: true }}
-      className={classes.swipe}
-    >
-      {images.map((image, idx) => (
-        <SwiperSlide key={idx} className={classNames(classes.image)}>
-          <Image
-            src={`http://photos.hotelbeds.com/giata/bigger/${image.path}`}
-            alt="hotel image"
-            width="700"
-            height="500"
-            className={classNames(classes.image__img)}
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation={true}
+        pagination={{ dynamicBullets: true }}
+        className={classes.swipe}
+      >
+        {images.map((image, idx) => (
+          <SwiperSlide key={idx} className={classNames(classes.image)}>
+            <Image
+              src={`http://photos.hotelbeds.com/giata/bigger/${image.path}`}
+              alt="hotel image"
+              width="700"
+              height="500"
+              className={classNames(classes.image__img)}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {/* <div ref={(node) => setPrevEl(node)}>prev</div>
+      <div ref={(node) => setNextEl(node)}>next</div> */}
+    </>
   );
 };
 
